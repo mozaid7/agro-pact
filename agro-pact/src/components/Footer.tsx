ad554f2f46f9c444295df369a9fa21686c50b37c
Rahul Sahani <yourahul1917@gmail.com>
2024-09-15 18:42:59 +0530

    responsive hero

--- agro-pact/src/components/Footer.tsx
+++ agro-pact/src/components/Footer.tsx
@@ -2,17 +2,43 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='h-auto w-full  mx-24 bg-zinc-900 text-white rounded-2xl'>
-      <div className='p-16'>
+    <section className='h-auto w-full bg-[#1e1b19] text-white rounded-3xl'>
+      <div className='p-12'>
         
-        <div className='grid grid-col-4'>
-          <div>
+        <div className='grid grid-cols-4 *:w-fit *:h-auto'>
+          <div className=''>
           <h1>Company</h1>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit quia distinctio et sint? Quod corrupti, est temporibus obcaecati quasi rem sit at quidem debitis odit perferendis voluptatem a labore repellendus?</p>
           </div>
+          <div className='items-center justify-center flex flex-col mx-auto'>
+            <h1>Services</h1>
+            <ul>
+              <li>Service 1</li>
+              <li>Service 2</li>
+              <li>Service 3</li>
+              <li>Service 4</li>
+              </ul>
+          </div>
+          <div className='items-center justify-center flex flex-col mx-auto'>
+            <h1>Company</h1>
+            <ul>
+              <li>Cases</li>
+              <li>Our Resources</li>
+              <li>About Us</li>
+              </ul>
+          </div>
+          <div className='items-center justify-center flex flex-col mx-auto'>
+            <h1>Contact Information</h1>
+            <ul>
+              <li>email</li>
+              <li>phone</li>
+              </ul>
+          </div>
         </div>
         <div></div>
+      <div className='bg-zinc-700 h-[1px] w-full my-4'></div>
       </div>
+
     </section>
   )
 }
---
+++ agro-pact/src/components/Services.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Services = () => {
+    return (<>
+        <div className='relative min-h-screen w-full grid grid-cols-5 -top-6 -z-50 opacity-45'>
+            <div className='border-x-2 border-dotted border-[#bba488]'></div>
+            <div></div>
+            <div className='border-x-2 border-dotted border-[#bba488]'></div>
+            <div></div>
+            <div className='border-x-2 border-dotted border-[#bba488]'></div>
+        </div>
+    </>
+    )
+}
+
+export default Services
