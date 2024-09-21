5517d99788250850a7b9a0edd70c0ea8a3f97ac4
Rahul Sahani <yourahul1917@gmail.com>
2024-09-21 22:49:55 +0530

    feature section update

--- agro-pact/src/components/FeaturesBento.tsx
+++ agro-pact/src/components/FeaturesBento.tsx
@@ -2,6 +2,11 @@ import { CalendarIcon, FileTextIcon } from "@radix-ui/react-icons";
 import { BellIcon, Share2Icon } from "lucide-react";
 import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
 import { AnimatedBeamMultipleOutput } from "./ui/animated-beam-multiple-output";
+import { ZapIllustration } from "./ui/zap";
+import { Calendar } from "./ui/calender";
+import React from "react";
+
+const [date, setDate] = React.useState<Date | undefined>(new Date())
 
 const features = [
   {
@@ -12,32 +17,14 @@ const features = [
     cta: "Learn more",
     className: "col-span-3 lg:col-span-1",
     background: (
-      // <Marquee
-      //   pauseOnHover
-      //   className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
-      // >
-      //   {files.map((f, idx) => (
-      //     <figure
-      //       key={idx}
-      //       className={cn(
-      //         "relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
-      //         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-      //         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-      //         "transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none",
-      //       )}
-      //     >
-      //       <div className="flex flex-row items-center gap-2">
-      //         <div className="flex flex-col">
-      //           <figcaption className="text-sm font-medium dark:text-white ">
-      //             {f.name}
-      //           </figcaption>
-      //         </div>
-      //       </div>
-      //       <blockquote className="mt-2 text-xs">{f.body}</blockquote>
-      //     </figure>
-      //   ))}
-      // </Marquee>
-      <div></div>
+      <>
+      <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
+      </>
     ),
   },
   {
@@ -71,7 +58,9 @@ const features = [
     href: "#",
     cta: "Learn more",
     background: (
-      <div></div>
+      <div>
+        <ZapIllustration />
+      </div>
     ),
   },
 ];
