875f96e4ef74f98caf8a9af1ef04ed15573b4596
Rahul Sahani <yourahul1917@gmail.com>
2024-09-21 15:36:11 +0530

    feature section update

--- agro-pact/src/components/FeaturesBento.tsx
+++ agro-pact/src/components/FeaturesBento.tsx
@@ -1,68 +1,89 @@
-import {
-  BellIcon,
-  CalendarIcon,
-  FileTextIcon,
-  GlobeIcon,
-  InputIcon,
-} from "@radix-ui/react-icons";
-
+import { CalendarIcon, FileTextIcon } from "@radix-ui/react-icons";
+import { BellIcon, Share2Icon } from "lucide-react";
 import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
+import { AnimatedBeamMultipleOutput } from "./ui/animated-beam-multiple-output";
 
 const features = [
   {
     Icon: FileTextIcon,
     name: "Save your files",
     description: "We automatically save your files as you type.",
-    href: "/",
+    href: "#",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" alt="" />,
-    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
+    className: "col-span-3 lg:col-span-1",
+    background: (
+      // <Marquee
+      //   pauseOnHover
+      //   className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
+      // >
+      //   {files.map((f, idx) => (
+      //     <figure
+      //       key={idx}
+      //       className={cn(
+      //         "relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
+      //         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+      //         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+      //         "transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none",
+      //       )}
+      //     >
+      //       <div className="flex flex-row items-center gap-2">
+      //         <div className="flex flex-col">
+      //           <figcaption className="text-sm font-medium dark:text-white ">
+      //             {f.name}
+      //           </figcaption>
+      //         </div>
+      //       </div>
+      //       <blockquote className="mt-2 text-xs">{f.body}</blockquote>
+      //     </figure>
+      //   ))}
+      // </Marquee>
+      <div></div>
+    ),
   },
   {
-    Icon: InputIcon,
-    name: "Full text search",
-    description: "Search through all your files in one place.",
-    href: "/",
+    Icon: BellIcon,
+    name: "Notifications",
+    description: "Get notified when something happens.",
+    href: "#",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" alt=""/>,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+    className: "col-span-3 lg:col-span-2",
+    background: (
+      // <AnimatedListDemo className="absolute right-2 top-4 h-[300px] w-full border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] group-hover:scale-105" />
+      <div></div>
+    ),
   },
   {
-    Icon: GlobeIcon,
-    name: "Multilingual",
-    description: "Supports 100+ languages and counting.",
-    href: "/",
+    Icon: Share2Icon,
+    name: "Integrations",
+    description: "Supports 100+ integrations and counting.",
+    href: "#",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" alt=""/>,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
+    className: "col-span-3 lg:col-span-2",
+    background: (
+      <AnimatedBeamMultipleOutput className="absolute right-2 top-4 h-[300px] border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_50%)] group-hover:scale-105" />
+    ),
   },
   {
     Icon: CalendarIcon,
     name: "Calendar",
     description: "Use the calendar to filter your files by date.",
-    href: "/",
+    className: "col-span-3 lg:col-span-1",
+    href: "#",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" alt=""/>,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
-  },
-  {
-    Icon: BellIcon,
-    name: "Notifications",
-    description:
-      "Get notified when someone shares a file or mentions you in a comment.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" alt="" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
+    background: (
+      <div></div>
+    ),
   },
 ];
 
 export default function FeaturesBento() {
   return (
-    <BentoGrid className="lg:grid-rows-2 ">
+    <div className="px-56 h-auto w-full flex justify-center items-center">
+    <BentoGrid>
       {features.map((feature) => (
         <BentoCard key={feature.name} {...feature} />
       ))}
     </BentoGrid>
+      </div>
   );
 }
