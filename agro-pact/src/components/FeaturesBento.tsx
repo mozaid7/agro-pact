d80e467a7d9d0c9095c588959d0feca2e861246c
Rahul Sahani <yourahul1917@gmail.com>
2024-09-23 11:08:26 +0530

    completed features bento component

--- agro-pact/src/components/FeaturesBento.tsx
+++ agro-pact/src/components/FeaturesBento.tsx
@@ -3,40 +3,33 @@ import { BellIcon, Share2Icon } from "lucide-react";
 import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
 import { AnimatedBeamMultipleOutput } from "./ui/animated-beam-multiple-output";
 import { ZapIllustration } from "./ui/zap";
-import { Calendar } from "./ui/calender";
-import React from "react";
+import { AnimatedListNotifications } from "./FeatureNotification";
+import { CommandMenu } from "./ui/command-menu";
 
-const [date, setDate] = React.useState<Date | undefined>(new Date())
 
 const features = [
-  {
-    Icon: FileTextIcon,
-    name: "Save your files",
-    description: "We automatically save your files as you type.",
-    href: "#",
-    cta: "Learn more",
-    className: "col-span-3 lg:col-span-1",
-    background: (
-      <>
-      <Calendar
-    mode="single"
-    selected={date}
-    onSelect={setDate}
-    className="rounded-md border"
-  />
-      </>
-    ),
-  },
   {
     Icon: BellIcon,
     name: "Notifications",
     description: "Get notified when something happens.",
     href: "#",
     cta: "Learn more",
+    className: "col-span-3 lg:col-span-1",
+    background: (
+      <AnimatedListNotifications className="absolute right-2 top-4 h-[300px] w-full border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] group-hover:scale-105" />
+    ),
+  },
+  {
+    Icon: BellIcon,
+    name: "Dashboard",
+    description: "Something happens.",
+    href: "#",
+    cta: "Learn more",
     className: "col-span-3 lg:col-span-2",
     background: (
-      // <AnimatedListDemo className="absolute right-2 top-4 h-[300px] w-full border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] group-hover:scale-105" />
-      <div></div>
+      <div className="absolute right-2 pt-6 w-full border-none transition-all duration-300 ease-out  group-hover:scale-105">
+        <CommandMenu />
+      </div>
     ),
   },
   {
@@ -58,7 +51,7 @@ const features = [
     href: "#",
     cta: "Learn more",
     background: (
-      <div>
+      <div className="absolute right-2 top-4 h-[300px] w-full border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_60%)] group-hover:scale-105">
         <ZapIllustration />
       </div>
     ),
